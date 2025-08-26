9bc9c6c41402ca35b8db75fa967dc5b2c096ef80
Adityakeerti <adityaanurag461@gmail.com>
2025-08-26 19:51:59 +0530

    Cleaned repo: keep only frontend for GitHub Pages

--- scripts/start_frontend.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Starting MariThon Frontend Server...
-cd /d "F:\CODING\MariThon\marithon_frontend"
-echo Starting HTTP server on http://localhost:8080
-python -m http.server 8080
-pause
